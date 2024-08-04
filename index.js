4367f312d1bce49c2271335da81efd02fd98cf43
Bernardo <service.bsfg@gmail.com>
2024-08-04 14:52:40 -0500

    testing code grade

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
-}
+// This function should be the one that sets up the event listener
+  function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', clickAlert);
+  }
+  function clickAlert() {
+    alert('I was clicked!');
+  }
+  //test
